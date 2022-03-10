cfaac72617a3c75dc4ec674c78adf86a38f4a6d8
Bru-mc <brunogmacau@gmail.com>
2022-03-10 20:20:45 -0300

    update pokemonScreen tsx and add axios and react-query

--- src/PokemonScreen.tsx
+++ src/PokemonScreen.tsx
@@ -1,17 +1,30 @@
 import React from 'react';
 import pikachu from './pngegg.png';
 import './PokemonScreen.css';
+import { useQuery } from 'react-query';
+import axios from 'axios';
 
-export class PokemonScreen extends React.Component {
-    render(){
-        return(
-            <div className='card'>
+export function PokemonScreen (){ 
+
+    const { data, isFetching } = useQuery<any>(`pokemonList`,async () => {
+        const response = await axios.get('https://pokeapi.co/api/v2/pokemon');
+        
+        return response.data;
+    })
+    console.log(data)
+    return(
+        
+        <div className='card'>
+            
+            
+            {data.results.map((pokemonConfig: { name: String, url: String}) =>{
+                return <li>{pokemonConfig.name}</li>
+            })}
             <h1 className='titulo'>Pikachu</h1>
             <img src={pikachu} alt="pikachu" />
             <p className='descricao'>
             Pokemon raro do tipo eletrico
             </p>
-            </div>
-        );
-    }
+        </div>
+    );
 }
