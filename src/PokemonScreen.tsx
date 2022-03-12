b3fe8293f7b6fdecb2a07d5cc7641b88f0ff1ddc
Bru-mc <brunogmacau@gmail.com>
2022-03-11 21:26:13 -0300

    Update PokemonScreen tsx/css

--- src/PokemonScreen.tsx
+++ src/PokemonScreen.tsx
@@ -4,27 +4,63 @@ import './PokemonScreen.css';
 import { useQuery } from 'react-query';
 import axios from 'axios';
 
+type pokeList = {
+    count: number,
+    next: string,
+    previous: any,
+    results: [{name: string, url: string}]
+}
 export function PokemonScreen (){ 
 
-    const { data, isFetching } = useQuery<any>(`pokemonList`,async () => {
-        const response = await axios.get('https://pokeapi.co/api/v2/pokemon');
-        
+    const baseUrl: string = 'https://pokeapi.co/api/v2';
+    const totalPokeId: number = 150;
+    let pokemonList: string= '';
+    const { data, isFetching } = useQuery<pokeList>(`pokemonList`,async () => {
+        const response = await axios.get(baseUrl+ `/pokemon?limit=150`);
         return response.data;
-    })
-    console.log(data)
+            // for(let pokeId = 1; pokeId <= totalPokeId; pokeId++){
+            //     const response = await axios.get(baseUrl+ `/pokemon/${pokeId}`);
+            //     let name: string = response.data.name;
+            //     console.log("Cheguei aqui");
+            //     let type: string = response.data.types.map((typeArray: {slot:Number, type:{name:String, url:String}}) =>{ return `<li>${typeArray.type.name}</li>`})
+            //     let image: string = response.data.sprites.other.dream_world.front_default;
+            //     pokemonList+= 
+            //     `<div className='card'> 
+            //         <h1 className='titulo'>${name}</h1>
+            //         <img src=${image} alt="${name}" />
+            //         <div className='poketype'> 
+            //             <h2 className='properties'>Tipo</h2>
+            //             <ul className='typeList'>
+            //                 ${type}
+            //             </ul>
+            //         </div>
+            //     </div>`
+            //     return response.data
+           
+            
+
+            
+                
+        })
+    console.log(`data = ${data}`)
+
     return(
+        <ul className='pokemonList'>
+            { data?.results.map<JSX.Element>(result =>{ return <li className = {result.name}>{result.name}</li>})}
+        </ul>
+    //     //data
         
-        <div className='card'>
+    //     // <div className='card'>
             
             
-            {data.results.map((pokemonConfig: { name: String, url: String}) =>{
-                return <li>{pokemonConfig.name}</li>
-            })}
-            <h1 className='titulo'>Pikachu</h1>
-            <img src={pikachu} alt="pikachu" />
-            <p className='descricao'>
-            Pokemon raro do tipo eletrico
-            </p>
-        </div>
+    //     //     {/* {data.results.map((pokemonConfig: { name: String, url: String}) =>{
+    //     //         return <li>{pokemonConfig.name}</li>
+    //     //     })} */}
+    //     //     <h1 className='titulo'>Pikachu</h1>
+    //     //     <img src={pikachu} alt="pikachu" />
+    //     //     <p className='descricao'>
+    //     //     Pokemon raro do tipo eletrico
+    //     //     </p>
+    //     // </div>
     );
 }
