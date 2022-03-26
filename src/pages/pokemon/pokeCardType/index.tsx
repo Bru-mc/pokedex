6ac6525c21a12cd2814216aa9d1687a64811a956
Bru-mc <brunogmacau@gmail.com>
2022-03-26 18:07:57 -0300

    Update card and animation

--- src/pages/pokemon/pokeCardType/index.tsx
+++ src/pages/pokemon/pokeCardType/index.tsx
@@ -4,9 +4,6 @@ import './index.css';
 export const PokemonCardType = (props:pokemonPropertys) =>{
   return(
     <div className="pokeCardType">
-      <div className='pokeballMiddleContainer'>
-        <div className='pokeballMiddleBottom'></div>
-      </div>
       <h2 className="pokeCardH2">TYPE :</h2>
       <div className="typeOp">
         {props.types?.map<JSX.Element>(types =>{
