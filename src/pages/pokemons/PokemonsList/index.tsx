97e18656e6feabcbbb5464a03eedd8bd55920913
Bru-mc <brunogmacau@gmail.com>
2022-03-28 09:44:30 -0300

    Update css and pokemonList

--- src/pages/pokemons/PokemonsList/index.tsx
+++ src/pages/pokemons/PokemonsList/index.tsx
@@ -9,31 +9,35 @@ import { useContext} from "react";
 
 
 export const PokemonsList = () => {
-    const { data, isFetching } = useQuery<pokeList>(`pokemonList`, () => pokemonsQuery(constants.pokeListApiUrl));
-    const {pokemonSeen} = useContext(PokemonContext)
-    const alreadySeen = (pokeName:string) =>{
-      if(pokemonSeen[pokeName]){
-        return <></>
-      }
-      else{
-        return <div className='newIcon'>
-          <p>NEW</p>
-        </div>
-      }
+  const {pokemonSeen} = useContext(PokemonContext)
+  const { data, isFetching } = useQuery<pokeList>(`pokemonList`, () => pokemonsQuery(constants.pokeListApiUrl));
+  
+  if(isFetching){
+    return <p>Loading...</p>
+  }
+  const alreadySeen = (pokeName:string) =>{
+    if(pokemonSeen[pokeName]){
+      return <></>
     }
-    return(
-        <ul className="pokemonList">
-            { data?.results.map<JSX.Element>(result =>( 
-                <Link to={`/pokemons/${result.name}`} key={result.name}>
-                  <li className = {result.name}>
-                    {result.name.toUpperCase()}
-                    {alreadySeen(result.name)}
-                    {/* <div className='newIcon'>
-                      <p>NEW</p>
-                    </div> */}
-                  </li>
-                </Link>
-            ))}
-        </ul>
-    );
+    else{
+      return <div className='newIcon'>
+        <p>NEW</p>
+      </div>
+    }
+  }
+  return(
+    <ul className="pokemonList">
+      { data?.results.map<JSX.Element>((result, index) =>( 
+        <Link to={`/pokemons/${result.name}`} key={result.name}>
+          <li className = {result.name}>
+            {`${index+1} ${result.name.toUpperCase()}`}
+            {alreadySeen(result.name)}
+            {/* <div className='newIcon'>
+              <p>NEW</p>
+            </div> */}
+          </li>
+        </Link>
+      ))}
+    </ul>
+  );
 } 
