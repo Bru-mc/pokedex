420814ad2ece1d8972fa9dc7f60a7f2e90955cb9
Bru-mc <brunogmacau@gmail.com>
2022-04-04 13:50:39 -0300

    Create Loading component

---
+++ src/components/Loading/index.tsx
@@ -0,0 +1,10 @@
+import "index.css";
+
+export const Loading = () => {
+    
+    return(
+        <div className="loadingContainer">
+            <div className="spinnerLoad"></div>
+        </div>
+    );
+}
