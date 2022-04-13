f968a6298926390ba40faaa6bdf7211c24cc59ec
Bru-mc <brunogmacau@gmail.com>
2022-04-13 15:46:16 -0300

    Update Pokedex, complete right side

--- src/Pokedex.tsx
+++ src/Pokedex.tsx
@@ -207,6 +207,21 @@ function Pokedex() {
               </div>
             </div>
           </div>
+          <div className="pokedexRightSideBottomContent">
+            <div className="lineLedsRightSide flex">
+              <div className="lineLedRightSide"></div>
+              <div className="lineLedRightSide"></div>
+            </div>
+            <div className="RS_BottomCircularLed circle yellow"></div>
+            <div className="pokedexRightSideBottomButtons flex">
+              <div className="RS_BButton"></div>
+              <div className="RS_BButton"></div>
+            </div> 
+            <div className="pokedexRightSideBottomScreens flex">
+              <div className="RS_BScreen"></div>
+              <div className="RS_BScreen"></div>
+            </div> 
+          </div>
           <div className="leftBorder backColor">
             <div className="topBorder"></div>
             <div className="bottomBorder"></div>
